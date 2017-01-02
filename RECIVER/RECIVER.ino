cefd5a2a6c7074fae6a1cf8d4dcdd7ea6ef6ae56
Song <songmingda1234@hotmail.com>
2017-01-02 22:42:02 +0800

    modified:   RECIVER/RECIVER.ino 	modified:   RECIVER/RECIVER.txt 	modified:   REMOTE/REMOTE.ino 	modified:   REMOTE/REMOTE.txt

--- RECIVER/RECIVER.ino
+++ RECIVER/RECIVER.ino
@@ -4,16 +4,16 @@
 #include <MirfHardwareSpiDriver.h>
 #include "Servo_mod.h"
 
-#define PAYLOAD 4
+#define PAYLOAD 5
 
 Servo powerServo[PAYLOAD];
+
 void setup()
 {
-  Serial.begin(9600);//Debug Mode
+  Serial.begin(115200);//Debug Mode
   Mirf.cePin = 8;//CE=D8
   Mirf.csnPin = 7;//CSN=D7
   Mirf.spi = &MirfHardwareSpi;
-  Mirf.channel = 24;//Channel
   Mirf.init();//Initialize
   Mirf.setRADDR((byte *)"RX01");
   Mirf.payload = PAYLOAD * sizeof(short); //Bytes at a time
@@ -22,20 +22,34 @@ void setup()
   powerServo[1].attach(5);//RUD
   powerServo[2].attach(3);//AIL
   powerServo[3].attach(4);//ELE
+  pinMode(10, OUTPUT);
 }
 
 void loop()
 {
   short Rdata[PAYLOAD];
+  long ti;
+  byte i;
   while (1)
   {
     if (!Mirf.isSending() && Mirf.dataReady()) //Get Data Via 24L01
     {
       Mirf.getData((byte *)&Rdata);
-      for (unsigned short i = 0; i < PAYLOAD; i++)
+      for (i = 0; i < PAYLOAD; i++)
       {
         powerServo[i].writeMicroseconds(Rdata[i]);//Drive ESCs
+        Serial.print(Rdata[i]);
+        Serial.print(" | ");
       }
+      digitalWrite(10, Rdata[4]);
+      Serial.println();
+      ti = millis();
+    }
+    if(millis() - ti > 2000)
+    {
+      for(i = 0; i < PAYLOAD; i++)
+        powerServo[i].writeMicroseconds(1000);
+      while(1);
     }
   }
 }
--- REMOTE/REMOTE.ino
+++ REMOTE/REMOTE.ino
@@ -2,30 +2,66 @@
 #include<Mirf.h>
 #include<nRF24L01.h>
 #include<MirfHardwareSpiDriver.h>
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
 
-#define PAYLOAD 4
-#define MAXPWR 2000
+#define PAYLOAD 5
+#define MAXPWR 2010
 #define MINPWR 990
-
-#define THRmid 505
-#define RUDmid 508
-#define AILmid 506
-#define ELEmid 505
+#define FILTER_N 10
 
 typedef struct
 {
-  short THR, RUD, AIL, ELE;
-} Motor;
+  short THR, RUD, AIL, ELE, L;
+} Payloads;
 
-void Sends(Motor *final)//Send Data Via 24L01
+LiquidCrystal_I2C lcd(0x27, 16, 2);
+
+short Filter(int pins)
 {
-  short msg[PAYLOAD];
-  msg[0] = map(final->THR, 0, 1023, MINPWR, MAXPWR);
-  msg[1] = map(final->RUD, 0, 1023, MINPWR, MAXPWR);
-  msg[2] = map(final->AIL, 0, 1023, MINPWR, MAXPWR);
-  msg[3] = map(final->ELE, 0, 1023, MINPWR, MAXPWR);
+  int filter_sum = 0;
+  for (byte i = 0; i < FILTER_N; i++)
+    filter_sum += analogRead(pins);
+  return filter_sum / FILTER_N;
+}
+
+void display(short *final)
+{
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("T: ");
+  lcd.print(final[0]);
+  lcd.print("  R: ");
+  lcd.print(final[1]);
+
+  lcd.setCursor(0, 1);
+  lcd.print("A: ");
+  lcd.print(final[2]);
+  lcd.print("  E: ");
+  lcd.print(final[3]);
+}
+
+void Sends(Payloads *final)//Send Data Via 24L01
+{
+  static short msg[PAYLOAD], count = 0;
+  msg[0] = map(final->THR, 1, 717, MINPWR, MAXPWR);
+  msg[1] = map(final->RUD, 292, 1023, MINPWR, MAXPWR) - 48;
+  msg[2] = map(final->AIL, 217, 990, MINPWR, MAXPWR) + 14;
+  msg[3] = map(final->ELE, 144, 878, MINPWR, MAXPWR) + 23;
+  msg[4] = final->L;
+  for (byte i = 0; i < PAYLOAD - 1; i++)
+  {
+    if (msg[i] < MINPWR) msg[i] = MINPWR;
+    else if (msg[i] > MAXPWR) msg[i] = MAXPWR;
+  }
   Mirf.send((byte *)&msg);
   while (Mirf.isSending());
+  count++;
+  if (count >= 15)
+  {
+    display(msg);
+    count = 0;
+  }
 }
 
 void setup()
@@ -34,18 +70,20 @@ void setup()
   Mirf.csnPin = 10;//CSN=D10
   Mirf.spi = &MirfHardwareSpi;
   Mirf.init();//Initialize
-  Mirf.channel = 24;//Channel
   Mirf.setTADDR((byte *)"RX01");
   Mirf.payload = PAYLOAD * sizeof(short); //Bytes At A Time
   Mirf.config();
+  lcd.begin();
+  lcd.backlight();
 }
 
 void loop()
 {
-  Motor final;
-  final.THR = analogRead(A0) + (512 - THRmid); //Throttle
-  final.RUD = analogRead(A1) + (512 - RUDmid); //Rudder
-  final.AIL = analogRead(A2) + (512 - AILmid); //Aileron
-  final.ELE = analogRead(A3) + (512 - ELEmid); //Elevator
+  static Payloads final;
+  final.THR = 1023 - Filter(A1); //Throttle
+  final.RUD = Filter(A0); //Rudder
+  final.AIL = Filter(A2); //Aileron
+  final.ELE = Filter(A3); //Elevator
+  final.L = digitalRead(2);
   Sends(&final);
 }
